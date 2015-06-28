c35b3c856f06fd76a51daa9659f8fee2e5182d46
Villanuevand <andresvillanuevab@gmail.com>
2015-06-28 18:40:10 -0430

    Ejercicio P2P obligatorio, modulo 6

--- routes/index.js
+++ routes/index.js
@@ -10,5 +10,8 @@ router.get('/', function(req, res) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+router.get('/author', function (req, res){
+	res.render('author',{});
+});
 
 module.exports = router;
