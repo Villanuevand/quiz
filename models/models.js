95cbcd8f13e1b5d19dc098d556eb0b1dd8949ae8
Villanuevand <andresvillanuevab@gmail.com>
2015-07-03 21:31:49 -0430

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,12 +1,34 @@
 var path = require('path');
 
-//Cargar model ORM.
+// Postgres DATABASE_URL = postgres://user:passwd@host:port/database
+// SQLite   DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
+
+// Cargar Modelo ORM
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(null, null, null,{
-	dialect : 'sqlite', storage : 'quiz.sqlite'
-});
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+    { dialect:  protocol,
+        protocol: protocol,
+        port:     port,
+        host:     host,
+        storage:  storage,  // solo SQLite (.env)
+        omitNull: true      // solo Postgres
+    }
+);
+
 //Importar la definición de la tabla Quiz en quiz.js
-var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // Exportar definición de la tabla quiz.
 
 //sequelize.sync() crea e inicializa la tabla de preguntas en DB.
