5032386f370709b82cf11643b24bc76808fd1041
Villanuevand <andresvillanuevab@gmail.com>
2015-07-05 16:50:54 -0430

    Autoload de la DB

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -3,26 +3,43 @@
 */
 var models = require('../models/models.js');
 
-// GET /quizes/:id
-exports.show = function (req, res) {
-	models.Quiz.find(req.params.quizId).then(function (quiz){
-		res.render('quizes/show', {quiz : quiz});
-	});
-};
-
-// GET /quizes/:id/answer
-exports.answer = function (req, res) {
-	models.Quiz.find(req.params.quizId).then(function (quiz) {
-		if(req.query.respuesta === quiz.respuesta)
-			res.render('quizes/answer', {quiz: quiz, respuesta : '¡Correcto!'});
-		else
-			res.render('quizes/answer', {quiz: quiz, respuesta : '¡Incorrecto!'});
-	});
+// Autoload - Factoriza el código si ruta incluye :quizId
+exports.load = function (req, res, next, quizId) {
+    models.Quiz.find(quizId)
+        .then(function (quiz) {
+            if(quiz){
+                req.quiz = quiz;
+                next();
+            } else {
+                next( new Error('No existe el Quiz Id: '+quizId));
+            }
+        })
+        .catch(function (e) {
+            next(e);
+        });
 };
 
 // GET /index
 exports.index = function (req, res){
-    models.Quiz.findAll().then(function (quizes){
-        res.render('quizes/index.ejs',{quizes : quizes});
-    });
+    models.Quiz.findAll()
+        .then(function (quizes){
+            res.render('quizes/index.ejs',{quizes : quizes});
+        })
+        .catch(function(e){
+            next(e);
+        });
 };
+
+// GET /quizes/:id
+exports.show = function (req, res) {
+    res.render('quizes/show', {quiz : req.quiz});
+};
+
+// GET /quizes/:id/answer
+exports.answer = function (req, res) {
+    var resultado = '¡Incorrecto!';
+    if(req.query.respuesta === req.quiz.respuesta)
+        resultado = '¡Correcto!';
+    res.render('quizes/answer', {quiz: req.quiz, respuesta : resultado});
+};
+
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload  de comandos con quizId
+router.param('quizId', quizController.load); //autoload
+
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
